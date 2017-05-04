fb6598966a36c72c1197b3e1d806a04d0400ddb3
李尚倫 <b03505011@ntu.edu.tw>
2017-05-04 19:26:33 +0800

    Completely finished the stable line tracking and color detecting features to complish the mission

--- BYNN_withoutPC/BYNN_withoutPC.ino
+++ BYNN_withoutPC/BYNN_withoutPC.ino
@@ -8,6 +8,8 @@
    需調前進速度！
 */
 #include <Servo.h>
+//#include "sortingAndActionNoMove.h"
+//#include "sortingAndActionSharp.h"
 ///////////////////////////////////////////////////////////////
 ////////// Pins /////////////////
 ///////////////////////////////////////////
@@ -45,17 +47,20 @@ int systemOpenState = 0;
 int FverylowSpeed = 50;
 int FlowSpeed = 60;
 int FnormalSpeed = 65;
-int FfastSpeed = 80;
-int FbrustSpeed =85;
+int FfastSpeed = 70;
+int FbrustSpeed = 80;
+int BbrustSpeed = 10;
 int verybigDegree = 25;
 int bigDegree = 15;
-int smallDegree = 10;
+int smallDegree = 12;
 int verysmallDegree = 10;
 int middleDegree = 82;
+int breakcounter=0;
 //**********color********************
 boolean colorState = 0;
-int colorGapDown = 24000;
-int colorGapUp = 27000;
+int colorGapDown = 1680;
+int colorGapUp = 2030;
+int colornumber = 0;
 
 
 ///////////////////////////////////////////////////////////////
@@ -157,76 +162,11 @@ void receiveColor() {
   //Serial.print("color index: ");
   //Serial.println(green);
   if ((green > colorGapDown) && (green < colorGapUp)) {
-    colorState = 1;
-    checkMission();
-  }
-}
-
-
-///////////////////////////////////////////////////////////////
-//////////sortingAndAction////////////////
-///////////////////////////////////////////
-void sortingAndAction() {
-  //Serial.println(IRState);
-  IntIRState = IRState.toInt();
-  switch (IntIRState) {
-    case 1111 : //01111
-      //Serial.println("need very big left turn");
-      directionControl(verybigDegree);
-      speedControl(FverylowSpeed, 10);
-      break;
-    case 11110 :
-      //Serial.println("need very big right turn");
-      directionControl(-1 * verybigDegree);
-      speedControl(FverylowSpeed, 10);
-      break;
-    case 111 : //00111
-      // Serial.println("need big left turn");
-      directionControl(bigDegree);
-      speedControl(FlowSpeed, 10);
-      break;
-    case 11100 :
-      //Serial.println("need big right turn");
-      directionControl(-1 * bigDegree);
-      speedControl(FlowSpeed, 10);
-      break;
-    case 10111 :
-      //Serial.println("need small left turn");
-      directionControl(smallDegree);
-      speedControl(FnormalSpeed, 10);
-      break;
-    case 11101 :
-      //Serial.println("need small right turn");
-      directionControl(-1 * smallDegree);
-      speedControl(FnormalSpeed, 10);
-      break;
-    case 10011 :
-      //Serial.println("need very small left turn");
-      directionControl(verysmallDegree);
-      speedControl(FfastSpeed, 10);
-      break;
-    case 11001 :
-      //Serial.println("need very small right turn");
-      directionControl(-1 * verysmallDegree);
-      speedControl(FfastSpeed, 10);
-      break;
-    case 11011 :
-      //Serial.println("straight forward! ");
-      directionControl(0);
-      speedControl(FbrustSpeed, 10);
-      break;
-    case 0:  //00000
-      //Serial.println("all black, stop!.");
-      directionControl(0);
-      speedControl(10, 10);
-      break;
-    case 11111:
-      //Serial.println("all white, maintain direction!");
-      speedControl(FverylowSpeed, 10);
-      break;
-    default:
-      //Serial.println("WTF, not in the sort! maintain direction.");
-      speedControl(FverylowSpeed, 10);
+    colornumber++;
+    if (colornumber > 5) {
+      colorState = 1;
+      checkMission();
+    }
   }
 }
 
@@ -235,13 +175,9 @@ void sortingAndAction() {
 //////////checkMission////////////////
 ///////////////////////////////////////////
 void checkMission() {
-  Serial.println("mission complete !");
-  directionControl(0);
-  speedControl(10, 10);
-  while (1) {
-    Serial.println("waiting for shoutdown...");
-    delay(2000);
-  }
+  //Serial.println("mission complete !");
+  //Serial.println("waiting for shoutdown...");
+  sortingAndActionNoMove();
 }
 
 
@@ -261,79 +197,4 @@ void directionControl(int degree) { //左負右正
   int ddegree = middleDegree - 1 * degree;
   myServo.write(ddegree);
   //Serial.println(ddegree);
-}
-
-
-///////////////////////////////////////////////////////////////
-//////////color////////////////
-///////////////////////////////////////////
-/*
-  Eecher's TCS3200 program
-  adapted from code found at reibot.org
-
-  This section will return the pulseIn reading of the selected color.
-  It will turn on the sensor at the start taosMode(1), and it will power off the sensor at the end taosMode(0)
-  color codes: 0=white, 1=red, 2=blue, 3=green
-  if LEDstate is 0, LED will be off. 1 and the LED will be on.
-  taosOutPin is the ouput pin of the TCS3200.
-*/
-
-float colorRead(int taosOutPin) {
-  //turn on sensor and use highest frequency/sensitivity setting
-  taosMode(3);
-
-  //setting for a delay to let the sensor sit for a moment before taking a reading.
-  int sensorDelay = 50;
-
-  //set the S2 and S3 pins to select the color to be sensed
-  //(color == 3) {//green
-  digitalWrite(S3, HIGH); //S3
-  digitalWrite(S2, HIGH); //S2
-
-  // create a var where the pulse reading from sensor will go
-  float readPulse;
-
-  //  turn LEDs on or off, as directed by the LEDstate var (IKKE Frydenlund)
-  digitalWrite(LED, HIGH);
-
-  // wait a bit for LEDs to actually turn on, as directed by sensorDelay var
-  delay(sensorDelay);
-
-  // now take a measurement from the sensor, timing a low pulse on the sensor's "out" pin
-  readPulse = pulseIn(taosOutPin, LOW, 80000);
-
-  //if the pulseIn times out, it returns 0 and that throws off numbers. just cap it at 80k if it happens
-  if (readPulse < .1) {
-    readPulse = 80000;
-  }
-
-  //turn off color sensor and LEDs to save power
-  taosMode(0);
-
-  // return the pulse value back to whatever called for it...
-  return readPulse;
-}
-
-// Operation modes area, controlled by hi/lo settings on S0 and S1 pins.
-//setting mode to zero will put taos into low power mode. taosMode(0);
-
-void taosMode(int mode) {
-  if (mode == 0) {
-    //power OFF mode-  LED off and both channels "low"
-    digitalWrite(LED, LOW);
-    digitalWrite(S0, LOW); //S0
-    digitalWrite(S1, LOW); //S1
-  } else if (mode == 1) {
-    //this will put in 1:1, highest sensitivity
-    digitalWrite(S0, HIGH); //S0
-    digitalWrite(S1, HIGH); //S1
-  } else if (mode == 2) {
-    //this will put in 1:5
-    digitalWrite(S0, HIGH); //S0
-    digitalWrite(S1, LOW); //S1
-  } else if (mode == 3) {
-    //this will put in 1:50
-    digitalWrite(S0, LOW); //S0
-    digitalWrite(S1, HIGH); //S1
-  }
 }
---
+++ BYNN_withoutPC/sortingAndActionNoMove.ino
@@ -0,0 +1,17 @@
+///////////////////////////////////////////////////////////////
+//////////sortingAndActionNoMove////////////////
+///////////////////////////////////////////
+void sortingAndActionNoMove() {
+  FverylowSpeed = 10;
+  FlowSpeed = 10;
+  FnormalSpeed = 10;
+  FfastSpeed = 10;
+  FbrustSpeed = 10;
+  BbrustSpeed = 100;
+  breakcounter++;
+  if (breakcounter > 50) {
+    BbrustSpeed=10;
+  }
+}
+
+
